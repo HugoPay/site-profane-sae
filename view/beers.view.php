54a306dc998ec1a25a412fe85f81998fb6489f7c
HugoPay <hugopayeux@gmail.com>
2023-06-01 00:09:42 +0200

    Formulaire, Beers et Contact fonctionnel

--- view/beers.view.php
+++ view/beers.view.php
@@ -1,54 +1,83 @@
     <!-- Auteur: POTIER Bastien -->
-        <main>
+    <main>
             <h2>Nos mixtures</h2>
             <section>
                 <div>
-                    <img src="" alt=""> <!--AJOUTER MOCKUPS-->
-                    <div>
-                        <h3 class="beer_name">Anarchie Conforme</h3>
-                        <p class="description">Venue d'on ne sait où pour le plus grand plaisir de vos papilles, anarchie conforme est un breuvage que seul ceux qui osent s'y frotter ne peuvent qu'apprécier. Produit phare de Profane, cette bière blanche au goût de grenade vous fera voyer à travers un univers aussi inconnu que sa provenance.</p>
+                    <img src="public/images/anarchie_conforme2.png" alt="Bière Anarchie Conforme" class="mockup_biere"> <!--AJOUTER MOCKUPS-->
+                    <div class="beer_description">
+                        <h3 class="beer_name">Anarchie conforme</h3>
+                        <p class="description">
+                            Venue d'on ne sait où pour le plus grand plaisir de vos papilles, 
+                            Anarchie conforme est un breuvage que seul ceux qui osent s'y frotter ne peuvent qu'apprécier. 
+                            Produit phare de Profane, cette bière blanche au goût de grenade vous fera voyer à travers un univers aussi 
+                            inconnu que sa provenance.
+                            <br>
+                            <br>
+                            <a href="formulaire.html" class="commander">Commander</a>
+                        </p>
                     </div>
-                    <a href="commande.html">Commander</a>
                 </div>
             </section>
             <section>
                 <div>
-                    <img src="" alt=""> <!--AJOUTER MOCKUPS-->
-                    <div>
-                        <h3 class="beer_name">Chaos Ordonné</h3>
-                        <p class="description"></p>
+                    <img src="public/images/chaos_ordonné.png" alt="Bière Chaos Ordonné" class="mockup_biere"> <!--AJOUTER MOCKUPS-->
+                    <div class="beer_description">
+                        <h3 class="beer_name">Chaos ordonné</h3>
+                        <p class="description">
+                            D'un noir aussi profond que celui des abysses, Chaos ordonné saura vous surprendre 
+                            avec un doux goût de cerise. Une bière blanche en contraste avec son apparence, elle saura satisfaire 
+                            vos plaisirs les plus intimes.
+                            <br>
+                            <br>
+                            <a href="formulaire.html" class="commander">Commander</a>
+                        </p>
                     </div>
-                    <a class="order_button" href="commande.html">Commander</a>
                 </div>
             </section>
             <section>
                 <div>
-                    <img src="" alt=""> <!--AJOUTER MOCKUPS-->
-                    <div>
-                        <h3 class="beer_name">Folie Rationnelle</h3>
-                        <p class="description"></p>
+                    <img src="public/images/folie_rationnelle.png" alt="Bière Folie Rationnelle" class="mockup_biere"> <!--AJOUTER MOCKUPS-->
+                    <div class="beer_description">
+                        <h3 class="beer_name">Folie rationnelle</h3>
+                        <p class="description">
+                            Aussi étrange que terre à terre, Folie rationnelle saura donner du piquant à votre 
+                            journée. Avec son jus de groseille, cette bière est destinée aux amateurs de brevage acides.
+                            <br>
+                            <br>
+                            <a href="formulaire.html" class="commander">Commander</a>
+                        </p>
                     </div>
-                    <a class="order_button" href="commande.html">Commander</a>
                 </div>
             </section>
             <section>
                 <div>
-                    <img src="" alt=""> <!--AJOUTER MOCKUPS-->
-                    <div>
+                    <img src="public/images/desordre_arrange.png" alt="Bière Désordre Arrangé" class="mockup_biere"> <!--AJOUTER MOCKUPS-->
+                    <div class="beer_description">
                         <h3 class="beer_name">Désordre arrangé</h3>
-                        <p class="description"></p>
+                        <p class="description">
+                            Arriverez-vous à résister à Désordre arrangé ? Cette bière, la plus alcooisée de notre 
+                            catalogue, vous prendra au dépourvu. Malgré son léger goût de myrtille, ne pensez pas passer une soirée 
+                            calme pour autant.
+                            <br>
+                            <br>
+                            <a href="formulaire.html" class="commander">Commander</a>
+                        </p>
                     </div>
-                    <a class="order_button" href="commande.html">Commander</a>
                 </div>
             </section>
             <section>
                 <div>
-                    <img src="" alt=""> <!--AJOUTER MOCKUPS-->
-                    <div>
-                        <h3 class="beer_name">Discorde Hxarmonieuse</h3>
-                        <p class="description"></p>
+                    <img src="public/images/discorde_harmonieuse.png" alt="Bière Discorde Harmonieuse" class="mockup_biere"> <!--AJOUTER MOCKUPS-->
+                    <div class="beer_description">
+                        <h3 class="beer_name">Discorde harmonieuse</h3>
+                        <p class="description">
+                            Une envie de soirée entre amis ? En contraste avec le reste de la collection, 
+                            Discorde harmonieuse propose une vision plus festive des bières de Profane.
+                            <br>
+                            <br>
+                            <a href="formulaire.html" class="commander">Commander</a>
+                        </p>
                     </div>
-                    <a class="order_button" href="commande.html">Commander</a>
                 </div>
             </section>
         </main>
